afb1733c3dd4cbde6928b7c7f05dfe702a610786
Juan Manuel <gaussnet@gmail.com>
2023-10-20 13:38:10 -0300

    Se modifica crearPruebas

--- controllers/pruebas.js
+++ controllers/pruebas.js
@@ -1,13 +1,21 @@
 const Prueba= require('../models/prueba');
 
+
 const crearPrueba= async(tipo, puntaje) => {
-    prueba= new Prueba({tipo, puntaje});
+    return new Promise((resolve, reject) => {
+        prueba= new Prueba({tipo, puntaje});
 
-    await prueba.save();
+        prueba.save();
 
-    return prueba;
+        if(prueba) {
+            resolve(prueba);
+            return;
+        }
 
+        reject('No se pudo crear prueba');
+    });
 }
+
 
 const crearPruebas= async(pruebas) => {
     const {
@@ -23,16 +31,18 @@ const crearPruebas= async(pruebas) => {
         pruebaMatafuego
     }= pruebas;
 
-    const pFrenos= await crearPrueba(pruebaFrenos.tipo, pruebaFrenos.puntaje);
-    const pSuspension= await crearPrueba(pruebaSuspension.tipo, pruebaSuspension.puntaje);
-    const pLuces= await crearPrueba(pruebaLuces.tipo, pruebaLuces.puntaje);
-    const pMotor= await crearPrueba(pruebaMotor.tipo, pruebaMotor.puntaje);
-    const pAlineacion= await crearPrueba(pruebaAlineacion.tipo, pruebaAlineacion.puntaje);
-    const pContaminacion= await crearPrueba(pruebaContaminacion.tipo, pruebaContaminacion.puntaje);
-    const pTrenDelantero= await crearPrueba(pruebaTrenDelantero.tipo, pruebaTrenDelantero.puntaje);
-    const pTrenTrasero= await crearPrueba(pruebaTrenTrasero.tipo, pruebaTrenTrasero.puntaje);
-    const pCinturones= await crearPrueba(pruebaCinturones.tipo, pruebaCinturones.puntaje);
-    const pMatafuego= await crearPrueba(pruebaMatafuego.tipo, pruebaMatafuego.puntaje);
+   const [pFrenos, pSuspension, pLuces, pMotor, pAlineacion, pContaminacion, pTrenDelantero, pTrenTrasero, pCinturones, pMatafuego]= await Promise.all([
+        crearPrueba(pruebaFrenos.tipo, pruebaFrenos.puntaje),
+        crearPrueba(pruebaSuspension.tipo, pruebaSuspension.puntaje),
+        crearPrueba(pruebaLuces.tipo, pruebaLuces.puntaje),
+        crearPrueba(pruebaMotor.tipo, pruebaMotor.puntaje),
+        crearPrueba(pruebaAlineacion.tipo, pruebaAlineacion.puntaje),
+        crearPrueba(pruebaContaminacion.tipo, pruebaContaminacion.puntaje),
+        crearPrueba(pruebaTrenDelantero.tipo, pruebaTrenDelantero.puntaje),
+        crearPrueba(pruebaTrenTrasero.tipo, pruebaTrenTrasero.puntaje),
+        crearPrueba(pruebaCinturones.tipo, pruebaCinturones.puntaje),
+        crearPrueba(pruebaMatafuego.tipo, pruebaMatafuego.puntaje)
+    ]);
 
     const data= {
         pruebaFrenos: pFrenos._id, 
